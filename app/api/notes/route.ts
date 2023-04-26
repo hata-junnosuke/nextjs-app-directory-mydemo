89bca5b4fa35f6c529aa643526866c941150de90
hata-junnosuke <jh19911209@icloud.com>
2023-04-27 07:43:34 +0900

    [add/error]ノートの新規作成実装（POSTできない）

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -10,3 +10,12 @@ export async function GET() {
   const notes = await prisma.note.findMany();
   return NextResponse.json(notes)
 }
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 })
+}
